8c8bab64f7262bb65f1882ec3fd5127e6789799d
AMarozau <amarozau@slb.com>
2017-10-18 21:44:26 +0300

    FIx build

--- webpack.config.js
+++ webpack.config.js
@@ -2,13 +2,13 @@ const path = require('path');
 
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
-    template: './package-lock.jsonsrc/index.html',
+    template: 'src/index.html',
     filename: 'index.html',
     inject: 'body'
 });
 
 let config = {
-    entry: 'src/index.js',
+    entry: './src/index.js',
     output: {
         path: path.resolve('dist'),
         filename: 'bundle.js'
