d0bd3c0271ec0792342c317e7a80ee775b234e2c
AMarozau <amarozau@slb.com>
2017-10-23 23:23:26 +0300

    STABLE: Fix charts

--- src/components/rate-statistic/rate-statistic.jsx
+++ src/components/rate-statistic/rate-statistic.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Line } from "react-chartjs";
 
 import './rate-statistic.less';
 
@@ -9,33 +8,8 @@ export default class RateStatistic extends React.Component {
     }
 
     render() {
-        let data = {
-            labels: this.props.rateCurrencyStatistic.map(statistic => (new Date(statistic.date)).toUTCString()),
-            datasets: {
-                label: 'Month stats',
-                data: this.props.rateCurrencyStatistic.map(statistic => statistic.curOfficialRate),
-                backgroundColor: [
-                    'rgba(255, 99, 132, 0.2)',
-                    'rgba(54, 162, 235, 0.2)',
-                    'rgba(255, 206, 86, 0.2)',
-                    'rgba(75, 192, 192, 0.2)',
-                    'rgba(153, 102, 255, 0.2)',
-                    'rgba(255, 159, 64, 0.2)'
-                ],
-                borderColor: [
-                    'rgba(255,99,132,1)',
-                    'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)',
-                    'rgba(75, 192, 192, 1)',
-                    'rgba(153, 102, 255, 1)',
-                    'rgba(255, 159, 64, 1)'
-                ],
-                borderWidth: 1
-            }
-        };
         return (
             <div>
-                <Line data={data} width="600" height="250"/>
             </div>
         )
     }
